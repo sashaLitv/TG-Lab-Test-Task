336766ae6d4901eb350d30bf547c56ec7544def9
Sasha Lytvak <120226308+sashaLitv@users.noreply.github.com>
2026-09-14 05:26:45 +0300

    feat: implement video tracking with progress indication

--- predict_video.py
+++ predict_video.py
@@ -1,7 +1,9 @@
 import argparse
+import cv2
 from ultralytics import YOLO
 import os
 import datetime
+from tqdm import tqdm
 
 def predict_video(model_path: str, input_path: str, save_dir: str, **kwargs):
     '''Predicts objects in the video using the YOLO model and saves the output to the specified directory'''
@@ -21,8 +23,16 @@ def predict_video(model_path: str, input_path: str, save_dir: str, **kwargs):
         timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
         dynamic_name = f"{input_name}_{timestamp}"
 
-    results=model.predict(
+    cap = cv2.VideoCapture(input_path)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+    vid_stride = kwargs.get('stride', 1)
+    total_steps = total_frames // vid_stride if vid_stride > 0 else total_frames
+
+    results=model.track(
         source=input_path, 
+        tracker="bytetrack.yaml",
+        persist=True,
         save=True, 
         save_dir=os.path.abspath(save_dir),
         name=dynamic_name,
@@ -32,11 +42,12 @@ def predict_video(model_path: str, input_path: str, save_dir: str, **kwargs):
         stream=True,
         conf=kwargs.get('confidence_threshold', 0.5),
         iou=kwargs.get('iou_threshold', 0.7),
-        vid_stride=kwargs.get('video_stride', 1)
+        vid_stride=vid_stride
     )
 
-    for _ in results:
-        pass
+    with tqdm(total=total_steps, desc="Обробка відео", unit="кадр") as pbar:
+        for _ in results:
+            pbar.update(1)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run YOLO video tracking with ByteTrack and count unique birds.")
@@ -45,7 +56,7 @@ if __name__ == "__main__":
     parser.add_argument("--output_file", type=str, default=None, help="Name of the output video file (default: None, will use input name with timestamp)")
     parser.add_argument("--model", type=str, default="weights/best.pt", help="Path to trained YOLO model weights")
     parser.add_argument("--stride", type=int, default=1, help="Process every nth frame (default: 1)")
-    parser.add_argument("--conf", type=float, default=0.25, help="Confidence threshold (default: 0.5)")
+    parser.add_argument("--conf", type=float, default=0.5, help="Confidence threshold (default: 0.5)")
     parser.add_argument("--iou", type=float, default=0.7, help="IOU threshold for tracking (default: 0.7)")
     args = parser.parse_args()
 
@@ -56,5 +67,6 @@ if __name__ == "__main__":
         stride=args.stride,
         output_file=args.output_file,
         conf=args.conf,
+        iou=args.iou
     )
 
